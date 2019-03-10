1eba3cf442d2605225ebb0ae31f17fb1167946b0
Dhemes Mota <dhemes.mota@gmail.com>
2019-03-10 09:57:53 -0300

    category service

--- src/app/pages/categories/shared/category.service.ts
+++ src/app/pages/categories/shared/category.service.ts
@@ -23,12 +23,52 @@ export class CategoryService {
     );
   }
 
+  getById(id: number): Observable<Category> {
+    const url = `${this.apiPath}/${id}`;
+
+    return this.http.get(url).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToCategory)
+    );
+  }
+
+
+  create(category: Category): Observable<Category> {
+    return this.http.post(this.apiPath, category).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToCategory)
+    );
+  }
+
+
+  update(category: Category): Observable<Category> {
+    const url = `${this.apiPath}/${category.id}`;
+
+    return this.http.put(url, category).pipe(
+      catchError(this.handleError),
+      map(() => category)
+    );
+  }
+
+  delete(id: number): Observable<any> {
+    const url = `${this.apiPath}/${id}`;
+
+    return this.http.delete(url).pipe(
+      catchError(this.handleError),
+      map(() => null)
+    );
+  }
+
   // PRIVATE METHODS
 
   private jsonDataToCategories(jsonData: any[] ): Category[] {
     const categories: Category[] = [];
     jsonData.forEach(element => categories.push(element as Category));
     return categories;
+  }
+
+  private jsonDataToCategory(jsonData: any): Category {
+    return jsonData as Category;
   }
 
   private handleError(error: any): Observable<any> {
