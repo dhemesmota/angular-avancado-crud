3c9bd085864416b49d4f33b4ef810125f2b15a64
Dhemes Mota <dhemes.mota@gmail.com>
2019-03-10 09:56:26 -0300

    adding category service

---
+++ src/app/pages/categories/shared/category.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable, throwError } from 'rxjs';
+import { map, catchError, flatMap } from 'rxjs/operators';
+
+import { Category } from './category.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  // tslint:disable-next-line:no-inferrable-types
+  private apiPath: string = 'api/categories';
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<Category[]> {
+    return this.http.get(this.apiPath).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToCategories)
+    );
+  }
+
+  // PRIVATE METHODS
+
+  private jsonDataToCategories(jsonData: any[] ): Category[] {
+    const categories: Category[] = [];
+    jsonData.forEach(element => categories.push(element as Category));
+    return categories;
+  }
+
+  private handleError(error: any): Observable<any> {
+    console.log('ERRO NA REQUISIÇÃO =>', error);
+    return throwError(error);
+  }
+}
