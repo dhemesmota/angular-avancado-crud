ebbb1dfe4782a5c5aed3562202240520fea7e6be
Dhemes Mota <dhemes.mota@gmail.com>
2019-03-11 10:10:32 -0300

    adding EntryListComponent

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,10 +1,11 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
 
 import { Category } from './pages/categories/shared/category.model';
+import { Entry } from './pages/entries/shared/entry.model';
 
 export class InMemoryDatabase implements InMemoryDbService {
     createDb() {
-        const categories = [
+        const categories: Category[] = [
             { id: 1, name: 'Moradia', description: 'Pagamentos de Contas da Casa' },
             { id: 2, name: 'Sáude', description: 'Plano de Saúde e Remédios' },
             { id: 3, name: 'Lazer', description: 'Cinema, parques, praia, etc' },
@@ -12,8 +13,13 @@ export class InMemoryDatabase implements InMemoryDbService {
             { id: 5, name: 'Freelas', description: 'Trabalhos como freelancer' }
         ];
 
+        const entries: Entry[] = [
+        { id: 1, name: 'Gás de Cozinha', categoryId: categories[0].id, category: categories[0], paid: false, date: '14/10/2018', amount:'70,80', type: 'expense', description: 'Qualquer descrição para essa despesa'} as Entry,
+        { id: 2, name: 'Teste Cozinha', categoryId: categories[1].id, category: categories[1], paid: true, date: '16/11/2018', amount: '75,80', type: 'revenue', description: 'Qualquer descrição para essa despesa' } as Entry
+        ];
+
         // tslint:disable-next-line:semicolon
-        return { categories }
+        return { categories, entries }
     }
 // tslint:disable-next-line:eofline
 }
